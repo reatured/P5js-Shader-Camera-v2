5af7fe2c28ee445bb2dcf8793be126e07e3108c1
reatured <61943376+reatured@users.noreply.github.com>
2021-12-02 15:40:19 -0800

    Add Consent and hand signs

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@
 let theShader;
 // this variable will hold our webcam video
 let cam;
-let w 
+let w
 let h = 720
 
 let faceapi;
@@ -30,7 +30,13 @@ let bufferFrame = 0
 let predictions = [];
 let handpose;
 let detected = false
-
+let slider
+let slider2
+let unisonSignPos
+let unisonSignPos2
+let fingerTips = []
+let unisonSignImg
+let spinnedCount = 0;
 // by default all options are set to true
 const detection_options = {
   withLandmarks: true,
@@ -43,9 +49,17 @@ function preload() {
   // load the shader
   theShader = loadShader('assets/webcam.vert', 'assets/webcam.frag');
   sharingan = loadImage('assets/eyeball.png')
+  unisonSignImg = loadImage('assets/UnisonSign.png')
 }
 
 function setup() {
+  unisonSignPos = createVector(30, 100)
+
+
+  // slider = createSlider(-200, 200, 100);
+  // slider.position(1000, 10);
+  // slider2 = createSlider(-200, 200, 100);
+  // slider2.position(1000, 50);
   angleMode(DEGREES)
   noStroke()
   w = h * 4 / 3
@@ -60,8 +74,8 @@ function setup() {
 
   cam.hide();
   // translate(-w / 2, -h / 2)
-  
-  handpose = ml5.handpose(cam);
+
+  handpose = ml5.handpose(cam, loadFingers);
   handpose.on("predict", results => {
     predictions = results;
   });
@@ -71,9 +85,12 @@ function setup() {
 
   eyeLayer.translate(50, 50)
 }
+function loadFingers() {
+  console.log("loadFingers")
+}
 
-function draw(){
-  
+function draw() {
+
   background(220);
   eyeLayer.rotate(-0.3)
   eyeLayer.image(sharingan, -50, -50, 100, 100)
@@ -84,11 +101,16 @@ function draw(){
   theShader.setUniform('tex0', cam);
   theShader.setUniform("iResolution", [width, height]);
   theShader.setUniform("iFrame", myFrameCount);
-  if(detected == true){
+  if (detected == true && unisonSign() == true) {
     myFrameCount++
+    spinnedCount = 0
+  } else {
+    myFrameCount -= 10
+    myFrameCount = max(0, myFrameCount)
+    spinnedCount++
   }
   pg.rect(0, 0, width, height);
-  image(pg, 0,0)
+  image(pg, 0, 0)
 
   noStroke()
   fill('blue')
@@ -108,35 +130,69 @@ function draw(){
   theShader.setUniform("iMouse", [map(eyeLeft + eyeWidth / 2, 0, width, 0, 1), map(eyeTop + eyeWidth / 2, 0, height, 0, 1)]);
 
   drawKeypoints();
+  // console.log(slider.value())
+  displaySign()
 }
 
+function unisonSign() {//detect sign hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+  let result = false
+  if (fingerTips.length > 0) {
+
+    let d = abs(unisonSignPos.copy().add(eyeLeft, eyeTop).dist(fingerTips[1]))
+    let d2 = abs(unisonSignPos.copy().add(eyeLeft, eyeTop).dist(fingerTips[2]))
+
+    if (d < 48 && d2 < 48) {
+      result = true
+    }
+  }
+
+  return result
+}
+
+function displaySign() {
+  let x = unisonSignPos.x + eyeX
+  let y = unisonSignPos.y + eyeY
+
+  if (spinnedCount > 30) {
+    image(unisonSignImg, x - 100, y - 54)
+    push()
+    strokeWeight(2)
+    stroke('white')
+    fill('orange')
+    textSize(24)
+    text("Mix and Feel your Chakra", x - 40, y + 200)
+    pop()
+  }
+
+  // console.log(slider.value(), slider2.value())
+}
 
 function gotResults(err, result) {//update
   // console.log(predictions)
-  
+
   if (err) {
     console.log(err)
     return
   }
 
-  
+
   // console.log(result)
   detections = result;
 
   if (detections) {
     if (detections.length > 0) {
-      // detected = true
+      detected = true
       // let theText = detections[0].detection.score
       // console.log(theText)
       leftEye = detections[0].parts.leftEye;
 
-    } else{
+    } else {
       detected = false
-      myFrameCount = 0
+
     }
   }
 
-  
+
   if (mouseIsPressed) {
     if (detections) {
 
@@ -147,36 +203,37 @@ function gotResults(err, result) {//update
   }
 
 
-  
+
   // drawRect()
-  
+
   faceapi.detect(gotResults)
 }
 
-function drawRect(){
+function drawRect() {
   push()
   const keypoint = prediction.annotations.thumb[3];
-  rect(keypoint.x, keypoint.y, 100,   100)
+  rect(keypoint.x, keypoint.y, 100, 100)
   pop()
 }
 //============================End of my code ==================================
 
 function drawKeypoints() {
+  fingerTips = []
   for (let i = 0; i < predictions.length; i += 1) {
     const prediction = predictions[i];
-    for (let j = 0; j < prediction.annotations.thumb.length; j += 1) {
-      const keypoint = prediction.annotations.thumb[j];
-      fill(j*40, 255, 0);
+    for (let j = 4; j < prediction.landmarks.length; j += 4) {
+      const keypoint = prediction.landmarks[j];
+      fill(0, 255, 0);
       noStroke();
-      ellipse(keypoint[0], keypoint[1], 10, 10);
+      // ellipse(keypoint[0] * 1.5, keypoint[1] * 1.5, 10, 10);
+
+      fingerTips.push(createVector(keypoint[0] * 1.5, keypoint[1] * 1.5))
     }
-    text(int(prediction.annotations.thumb[0][1]), 100, 100)
   }
 }
 
 function modelReady() {
   console.log('ready!')
-  console.log(faceapi)
   faceapi.detect(gotResults)
 }
 
